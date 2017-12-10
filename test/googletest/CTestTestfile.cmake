1e4e96887420a1020359f91d9d3e0f20c38651f0
Andrew Garkavyi <andrew.gawy@gmail.com>
2017-12-09 20:23:56 -0800

    Added Google test framework

---
+++ test/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/andrew/Projects/ai-ml-course/SDC-T2/CarND-Unscented-Kalman-Filter-Project/test/googletest
+# Build directory: /Users/andrew/Projects/ai-ml-course/SDC-T2/CarND-Unscented-Kalman-Filter-Project/test/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
